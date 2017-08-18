b250e6e69865915a8e872286180e0051f7bc26e1
Daniel Domberger <queltos@gmail.com>
2017-08-18 21:27:15 +0200

    get user from django session

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,21 +1,23 @@
 import json
 from channels import Group
 from channels.sessions import channel_session
+from channels.auth import channel_session_user, channel_session_user_from_http
 from urllib.parse import parse_qs
 
 
-@channel_session
+@channel_session_user_from_http
 def ws_connect(message, room_name):
     message.reply_channel.send({"accept": True})
     params = parse_qs(message.content["query_string"])
     if b"username" in params:
         message.channel_session["username"] = params[b"username"][0].decode("utf8")
+        # TODO use message.user.username from django session (we're getting this from decorator)
         Group("chat-%s" % room_name).add(message.reply_channel)
     else:
         message.reply_channel.send({"close": True})
 
 
-@channel_session
+@channel_session_user
 def ws_message(message, room_name):
     Group("chat-%s" % room_name).send({
         "text": json.dumps({
@@ -25,7 +27,7 @@ def ws_message(message, room_name):
     })
 
 
-@channel_session
+@channel_session_user
 def ws_disconnect(message, room_name):
     Group("chat-%s" % room_name).discard(message.reply_channel)
 
