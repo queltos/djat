a557e3129a176a5b4c373ddd2e7868ba9cde77dc
Daniel Domberger <queltos@gmail.com>
2017-08-18 18:59:44 +0200

    remove commented code

--- djat/routing.py
+++ djat/routing.py
@@ -3,7 +3,6 @@ from Chat.consumers import ws_message, ws_connect, ws_disconnect
 
 
 channel_routing = [
-    # route("http.request", "Chat.consumers.http_consumer"),
     route("websocket.connect", ws_connect, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
     route("websocket.receive", ws_message, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
     route("websocket.disconnect", ws_disconnect, path=r"^/(?P<room_name>[a-zA-Z0-9_]+)/$"),
