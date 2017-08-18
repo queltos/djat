8c9c89e63c1f2ac15969c127334eb17fc7aa09b0
Daniel Domberger <queltos@gmail.com>
2017-08-18 20:25:15 +0200

    [client] rooms can now be joined with a username

--- Chat/static/Chat/client.js
+++ Chat/static/Chat/client.js
@@ -1,17 +1,31 @@
-socket = new WebSocket("ws://" + window.location.host + "/general/?username=hihi");
+var socket;
 
-socket.onmessage = function(e) {
-    var para = document.createElement("P");
-    var text = document.createTextNode(e.data);
-    para.appendChild(text);
-    document.getElementById("chat").appendChild(para);
-}
+function onJoin() {
+    var room = document.getElementById("roomInput").value;
+    var user = document.getElementById("userInput").value;
 
-socket.onopen = function() {
-    socket.send("hello world");
+    if(room && user) {
+        socket = new WebSocket("ws://" + window.location.host + "/" + room + "/?username=" + user);
+
+        socket.onmessage = function(e) {
+            var data = JSON.parse(e.data);
+            var para = document.createElement("P");
+            var text = document.createTextNode(data["username"] + ": " + data["text"]);
+            para.appendChild(text);
+            document.getElementById("chat").appendChild(para);
+        }
+
+        socket.onopen = function() {
+            var para = document.createElement("P");
+            var text = document.createTextNode(room);
+            para.appendChild(text);
+            document.getElementById("rooms").appendChild(para);
+        }
+    }
 }
 
 function onSend() {
-    // Call onopen directly if socket is already open
-    if (socket.readyState == WebSocket.OPEN) socket.onopen();
+    var msg = document.getElementById('msgInput').value;
+
+    if (socket.readyState == WebSocket.OPEN) socket.send(msg);
 }
