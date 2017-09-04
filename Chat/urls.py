1d9aa3eb7528bf302942585c793764dfe71e4537
Daniel Domberger <queltos@gmail.com>
2017-09-04 21:05:08 +0200

    create login page

--- Chat/urls.py
+++ Chat/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'login', views.auth, name='login'),
+    url(r'^login$', views.login_page, name='login'),
+    url(r'^auth$', views.auth, name='auth'),
+    url(r'^logout$', views.logout_page, name='logout'),
 ]
--- Chat/views.py
+++ Chat/views.py
@@ -1,19 +1,22 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 
 
-@login_required
+@login_required(login_url='/login')
 def index(request):
     context = {'user': request.user.username}
     return render(request, 'Chat/main.html', context)
 
 
+@require_POST
 def auth(request):
-    # username = request.POST['username']
-    # password = request.POST['password']
-    username = 'dan'
-    password = 'djangodjango'
+    print(request.POST)
+    username = request.POST['username']
+    password = request.POST['password']
     user = authenticate(
         request,
         username=username,
@@ -22,4 +25,14 @@ def auth(request):
     if user is not None:
         login(request, user)
         return redirect('/')
+    else:
+        return redirect('/login')
 
+
+def logout_page(request):
+    logout(request)
+    return HttpResponse()
+
+
+def login_page(request):
+    return render(request, 'Chat/login.html')
