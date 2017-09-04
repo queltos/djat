c7f133901b8630530230bf0ecafbff6220e01026
Daniel Domberger <queltos@gmail.com>
2017-09-04 17:58:21 +0200

    add login session

--- Chat/views.py
+++ Chat/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 
+@login_required
 def index(request):
-    context = {}
+    context = {'user': request.user.username}
     return render(request, 'Chat/main.html', context)
+
+
+def auth(request):
+    # username = request.POST['username']
+    # password = request.POST['password']
+    username = 'dan'
+    password = 'djangodjango'
+    user = authenticate(
+        request,
+        username=username,
+        password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect('/')
+
