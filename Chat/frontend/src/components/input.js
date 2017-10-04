6738f0a2595cf8fde3b184a024b9b46608273290
Daniel Domberger <queltos@gmail.com>
2017-10-04 17:01:59 +0200

    reformat input component

--- Chat/frontend/src/components/input.js
+++ Chat/frontend/src/components/input.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ChatInput = ({onInput}) => <div>
-  <input id="msgInput" onKeyDown={
-    function (event) {
-      if (event.keyCode === 13) {
-        console.log(`target.value is ${event.target.value}`)
-        onInput(event.target.value)
-        event.target.value = ""
+const ChatInput = ({onInput}) =>
+  <div>
+    <input id="msgInput" onKeyDown={(event) => {
+        if (event.keyCode === 13) {
+          onInput(event.target.value)
+          event.target.value = ""
+        }
       }
-    }
-  }/>
-</div>
+    }/>
+  </div>
 
 ChatInput.propTypes = {
   onSend: PropTypes.func
